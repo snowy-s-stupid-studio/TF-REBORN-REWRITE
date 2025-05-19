bb6cb94f47964099dd7c8d4dc6258450dddd0237
Snowy <157809252+Snowywasbanned@users.noreply.github.com>
2025-05-19 16:56:11 +0300

    changed the bat file to support raid and mapbase

--- src/createallprojects Raid Mode.bat
+++
@@ -1,1 +0,0 @@
-devtools\bin\vpc.exe /tf /define:SOURCESDK /define:DISCORD_RPC /define:TF_RAID_MODE +everything /mksln everything.sln
--- src/createallprojects.bat
+++ src/createallprojects.bat
@@ -1,1 +1,1 @@
-devtools\bin\vpc.exe /tf /define:SOURCESDK /define:DISCORD_RPC +everything /mksln everything.sln
+devtools\bin\vpc.exe /tf /define:SOURCESDK /define:DISCORD_RPC /define:TF_RAID_MODE /define:MAPBASE +everything /mksln everything.sln
