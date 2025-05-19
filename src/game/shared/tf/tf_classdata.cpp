f4cf691d1d61f4f10c054a6b8ec3b33628f21a04
Snowy <157809252+Snowywasbanned@users.noreply.github.com>
2025-05-19 14:58:08 +0300

    Update tf_classdata.cpp

--- src/game/shared/tf/tf_classdata.cpp
+++ src/game/shared/tf/tf_classdata.cpp
@@ -21,7 +21,7 @@ extern bool UseHWMorphModels();
 #define TF_CLASS_PYRO_FILE				"scripts/playerclasses/pyro"
 #define TF_CLASS_SPY_FILE				"scripts/playerclasses/spy"
 #define TF_CLASS_ENGINEER_FILE			"scripts/playerclasses/engineer"
-#define TF_CLASS_LOBSTER_FILE			"scripts/playerclasses/mobster"
+#define TF_CLASS_MOBSTER_FILE			"scripts/playerclasses/mobster"
 #define TF_CLASS_CIVILIAN_FILE			"scripts/playerclasses/civilian"
 
 const char *s_aPlayerClassFiles[] =
@@ -36,7 +36,7 @@ const char *s_aPlayerClassFiles[] =
 	TF_CLASS_PYRO_FILE,
 	TF_CLASS_SPY_FILE,
 	TF_CLASS_ENGINEER_FILE,
-	TF_CLASS_LOBSTER_FILE,
+	TF_CLASS_MOBSTER_FILE,
 	TF_CLASS_CIVILIAN_FILE
 };
 
