b58497f2564b2cb49a9136fed2161b0a980f7f00
birchish <Mooshrew.haze@gmail.com>
2025-05-23 18:00:40 -0400

    Fixing compilation issue with QC Eyes
    Changes in 11188d2 cause QC Eyes to fail to compile because it removed required dependencies. Reverting the changes that break it.
    
    If these changes cause jump pads (catapults) to break, I'll create a fix or revert.

--- src/utils/qc_eyes/StdAfx.h
+++ src/utils/qc_eyes/StdAfx.h
@@ -13,7 +13,12 @@
 
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 
+#include <afxext.h>         // MFC extensions
+#include <afxdisp.h>        // MFC Automation classes
+#include <afxdtctl.h>		// MFC support for Internet Explorer 4 Common Controls
+
 #ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>			// MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
 
