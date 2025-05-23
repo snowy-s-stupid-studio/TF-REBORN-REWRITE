71fa9c73963fd55325b20183e248f2ac23eb8a83
Snowy <157809252+Snowywasbanned@users.noreply.github.com>
2025-05-23 22:53:58 +0300

    basic vcds now we wait for vvis

--- src/game/shared/tf/tf_weapon_typewriter.cpp
+++ src/game/shared/tf/tf_weapon_typewriter.cpp
@@ -6,8 +6,6 @@
 #include "cbase.h"
 #include "tf_weapon_typewriter.h"
 
-static const float DAMAGE_TO_FILL_MINICRIT_METER = 100.0f;
-
 // Client specific.
 #ifdef CLIENT_DLL
 #include "c_tf_player.h"
@@ -29,36 +27,6 @@ CREATE_SIMPLE_WEAPON_TABLE( TFTYPEWRITER, tf_weapon_typewriter )
 BEGIN_DATADESC( CTFTYPEWRITER)
 END_DATADESC()
 #endif
-
-// ---------- Charged SMG -------------
-
-IMPLEMENT_NETWORKCLASS_ALIASED( TFChargedTYPEWRITER, DT_WeaponChargedTypewriter )
-
-BEGIN_NETWORK_TABLE( CTFChargedTYPEWRITER, DT_WeaponChargedTypewriter)
-// Client specific.
-#ifdef CLIENT_DLL
-RecvPropFloat( RECVINFO( m_flMinicritCharge ) ),
-// Server specific.
-#else
-SendPropFloat( SENDINFO( m_flMinicritCharge ), 4, SPROP_NOSCALE, 0.0f, DAMAGE_TO_FILL_MINICRIT_METER ),
-#endif
-END_NETWORK_TABLE()
-
-// Server specific
-#ifndef CLIENT_DLL
-BEGIN_DATADESC( CTFChargedTYPEWRITER)
-END_DATADESC()
-#endif
-
-// Client specific
-#ifdef CLIENT_DLL
-BEGIN_PREDICTION_DATA( CTFChargedTYPEWRITER)
-DEFINE_FIELD(  m_flMinicritCharge, FIELD_FLOAT )
-END_PREDICTION_DATA()
-#endif
-
-LINK_ENTITY_TO_CLASS( tf_weapon_charged_TYPEWRITER, CTFChargedTYPEWRITER);
-PRECACHE_WEAPON_REGISTER( tf_weapon_charged_TYPEWRITER);
 
 //=============================================================================
 //
@@ -95,104 +63,3 @@ bool CTFTYPEWRITER::CanFireCriticalShot( bool bIsHeadshot, CBaseEntity *pTarget 
 
 	return true;
 }
-
-//-----------------------------------------------------------------------------
-// Purpose:	Determine if secondary fire is available.
-//-----------------------------------------------------------------------------
-bool CTFChargedTYPEWRITER::CanPerformSecondaryAttack() const
-{
-	return ( m_flMinicritCharge >= DAMAGE_TO_FILL_MINICRIT_METER && BaseClass::CanPerformSecondaryAttack() );
-}
-
-//-----------------------------------------------------------------------------
-// Purpose: Determine whether to flash the HUD element showing the charge bar
-//-----------------------------------------------------------------------------
-bool CTFChargedTYPEWRITER::ShouldFlashChargeBar()
-{
-	return m_flMinicritCharge >= DAMAGE_TO_FILL_MINICRIT_METER;
-}
-
-//-----------------------------------------------------------------------------
-// Purpose: Get HUD charge bar progress amount
-//-----------------------------------------------------------------------------
-float CTFChargedTYPEWRITER::GetProgress( void )
-{
-	// Progress bar shows charge amount if we're charging up, otherwise drains over time if we're mini-crit boosted.
-	CTFPlayer *pPlayer = ToTFPlayer( GetOwner() );
-	if ( pPlayer && pPlayer->m_Shared.InCond( TF_COND_ENERGY_BUFF ) )
-	{
-		int flBuffDuration = 0;
-		CALL_ATTRIB_HOOK_FLOAT( flBuffDuration, minicrit_boost_when_charged );
-		if ( flBuffDuration > 0 )
-		{
-			float flElapsed = gpGlobals->curtime - m_flMinicritStartTime;
-			float flRemainingPortion = Clamp( (flBuffDuration - flElapsed) / flBuffDuration, 0.0f, 1.0f );
-			return flRemainingPortion;
-		}
-		else
-		{
-			return 0.0f;
-		}
-	}
-	else
-	{
-		return m_flMinicritCharge / DAMAGE_TO_FILL_MINICRIT_METER;
-	}
-}
-
-//-----------------------------------------------------------------------------
-// Purpose: Reset weapon state
-//-----------------------------------------------------------------------------
-void CTFChargedTYPEWRITER::WeaponReset()
-{
-	BaseClass::WeaponReset();
-	m_flMinicritCharge = 0.0f;
-	m_flMinicritStartTime = 0.0f;
-}
-
-//-----------------------------------------------------------------------------
-// Purpose: Perform secondary attack
-//-----------------------------------------------------------------------------
-void CTFChargedTYPEWRITER::SecondaryAttack()
-{
-	BaseClass::SecondaryAttack();
-
-	m_flMinicritCharge = 0.0f;
-
-	CTFPlayer *pPlayer = ToTFPlayer( GetOwner() );
-	if ( pPlayer )
-	{
-		float flBuffDuration = 0;
-		CALL_ATTRIB_HOOK_FLOAT( flBuffDuration, minicrit_boost_when_charged );
-		if ( flBuffDuration > 0 )
-		{
-			pPlayer->m_Shared.AddCond( TF_COND_ENERGY_BUFF, flBuffDuration );
-			m_flMinicritStartTime = gpGlobals->curtime;
-		}
-	}
-}
-
-#ifdef GAME_DLL
-//-----------------------------------------------------------------------------
-// Purpose: Update state when we score a hit with this weapon
-//-----------------------------------------------------------------------------
-void CTFChargedTYPEWRITER::ApplyOnHitAttributes( CBaseEntity *pVictimBaseEntity, CTFPlayer *pAttacker, const CTakeDamageInfo &info )
-{
-	BaseClass::ApplyOnHitAttributes( pVictimBaseEntity, pAttacker, info );
-	if ( pAttacker )
-	{
-		CTFPlayer *pPlayer = ToTFPlayer( GetOwner() );
-		if ( pPlayer && !pPlayer->m_Shared.InCond( TF_COND_ENERGY_BUFF ) )
-		{
-			float damage = info.GetDamage();
-			float flChargeRate = 0.0f;
-			CALL_ATTRIB_HOOK_FLOAT( flChargeRate, minicrit_boost_charge_rate );
-			m_flMinicritCharge += damage * flChargeRate;
-			if ( m_flMinicritCharge > DAMAGE_TO_FILL_MINICRIT_METER )
-			{
-				m_flMinicritCharge = DAMAGE_TO_FILL_MINICRIT_METER;
-			}
-		}
-	}
-}
-#endif
--- src/game/shared/tf/tf_weapon_typewriter.h
+++ src/game/shared/tf/tf_weapon_typewriter.h
@@ -1,6 +1,5 @@
 //========= Copyright Valve Corporation, All rights reserved. ============//
 //
-//
 //=============================================================================
 #ifndef TF_WEAPON_TYPEWRITER_H
 #define TF_WEAPON_TYPEWRITER_H
@@ -12,8 +11,11 @@
 
 // Client specific.
 #ifdef CLIENT_DLL
-#define CTFTYPEWRITER C_TFChargedTYPEWRITER
+#define CTFTYPEWRITER C_TFTYPEWRITER
 #define CTFChargedTYPEWRITER C_TFTYPEWRITER
+class C_TFTYPEWRITER; // Forward declaration for client builds
+#else
+#define CTFTYPEWRITER CTFTYPEWRITER // Ensure CTFTYPEWRITER is defined for server builds as well
 #endif
 
 //=============================================================================
@@ -24,11 +26,10 @@ class CTFTYPEWRITER : public CTFWeaponBaseGun
 {
 public:
 
-	DECLARE_CLASS( CTFTYPEWRITER, CTFWeaponBaseGun );
-	DECLARE_NETWORKCLASS(); 
+	DECLARE_CLASS(CTFTYPEWRITER, CTFWeaponBaseGun);
+	DECLARE_NETWORKCLASS();
 	DECLARE_PREDICTABLE();
 
-// Server specific.
 #ifdef GAME_DLL
 	DECLARE_DATADESC();
 #endif
@@ -36,58 +37,16 @@ public:
 	CTFTYPEWRITER() {}
 	~CTFTYPEWRITER() {}
 
-	virtual int		GetWeaponID( void ) const			{ return TF_WEAPON_SMG; }
+	virtual int		GetWeaponID(void) const { return TF_WEAPON_SMG; }
 
-	virtual int		GetDamageType( void ) const;
-	virtual bool	CanFireCriticalShot( bool bIsHeadshot, CBaseEntity *pTarget = NULL ) OVERRIDE;
+	virtual int		GetDamageType(void) const;
+	virtual bool	CanFireCriticalShot(bool bIsHeadshot, CBaseEntity* pTarget = NULL) OVERRIDE;
 
-	bool			CanHeadshot( void ) const { int iMode = 0; CALL_ATTRIB_HOOK_INT( iMode, set_weapon_mode ); return (iMode == 1); };
-
-private:
-
-	CTFTYPEWRITER( const CTFTYPEWRITER& ) {}
-};
-
-//=============================================================================
-//
-// TF Weapon Charged Sub-machine gun.
-//
-class CTFChargedTYPEWRITER : public CTFTYPEWRITER
-{
-public:
-	DECLARE_CLASS( CTFCharged_TYPEWRITER, CTFTYPEWRITER);
-	DECLARE_NETWORKCLASS();
-	DECLARE_PREDICTABLE();
-
-	// Server specific.
-#ifdef GAME_DLL
-	DECLARE_DATADESC();
-#endif
-
-	CTFChargedTYPEWRITER() {}
-	~CTFChargedTYPEWRITER() {}
-
-	virtual int		GetWeaponID( void ) const			{ return TF_WEAPON_CHARGED_TYPEWRITER; }
-
-	const char*		GetEffectLabelText( void ) { return "#TF_SmgCharge"; }
-	float			GetProgress( void );
-	bool			ShouldFlashChargeBar();
-	void			SecondaryAttack() OVERRIDE;
-	bool			CanPerformSecondaryAttack() const OVERRIDE;
-	void			WeaponReset() OVERRIDE;
-
-#ifdef GAME_DLL
-	void	ApplyOnHitAttributes( CBaseEntity *pVictimBaseEntity, CTFPlayer *pAttacker, const CTakeDamageInfo &info ) OVERRIDE;
-#endif
-
-protected:
-	CNetworkVar( float, m_flMinicritCharge );
-
-	float m_flMinicritStartTime;
+	bool			CanHeadshot(void) const { int iMode = 0; CALL_ATTRIB_HOOK_INT(iMode, set_weapon_mode); return (iMode == 1); };
 
 private:
-	CTFChargedTYPEWRITER( const CTFChargedTYPEWRITER& ) {}
-};
 
+	CTFTYPEWRITER(const CTFTYPEWRITER&) {}
+};
 
 #endif // TF_WEAPON_TYPEWRITER_H
